47120bd8d15f61320d4c5543115bc7c04c472d2e
Haichao Miao <miao@cg.tuwien.ac.at>
2018-08-15 10:17:38 -0400

    added empty creator classes

---
+++ AdenitaCoreSE/include/SEDSDNACreatorEditorDescriptor.hpp
@@ -0,0 +1,28 @@
+/// \headerfile SBProxy.hpp "SBProxy.hpp"
+#include "SBProxy.hpp"
+
+/// \headerfile SEDSDNACreatorEditor.hpp "SEDSDNACreatorEditor.hpp"
+#include "SEDSDNACreatorEditor.hpp"
+
+
+// Class descriptor
+
+// SAMSON Element generator pro tip: complete this descriptor to expose this class to SAMSON and other SAMSON Elements
+
+SB_CLASS_BEGIN(SEDSDNACreatorEditor);
+
+	SB_CLASS_TYPE(SBCClass::Editor);
+        SB_CLASS_DESCRIPTION("SEDSDNACreatorEditor : SAMSON Element generator pro tip: modify me");
+
+	SB_FACTORY_BEGIN;
+
+		SB_CONSTRUCTOR_0(SEDSDNACreatorEditor);
+
+	SB_FACTORY_END;
+
+	SB_INTERFACE_BEGIN;
+
+	SB_INTERFACE_END;
+
+SB_CLASS_END(SEDSDNACreatorEditor);
+
