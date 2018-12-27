a1c1cb3fa6c94e6cc8d9854aeba40e70424bdaf2
Elisa de Llano <elisa.dellano.fl@ait.ac.at>
2018-12-27 10:48:48 +0100

    Disabling un/serialization so it is possible to use samson saving features without crashing with Adenita

--- AdenitaCoreSE/source/ADNPart.cpp
+++ AdenitaCoreSE/source/ADNPart.cpp
@@ -23,98 +23,98 @@ void ADNPart::serialize(SBCSerializer * serializer, const SBNodeIndexer & nodeIn
 {
   //SBStructuralModel::serialize(serializer, nodeIndexer, sdkVersionNumber, classVersionNumber);
 
-  serializer->writeIntElement("number_ss", GetNumberOfSingleStrands());
+  //serializer->writeIntElement("number_ss", GetNumberOfSingleStrands());
 
-  serializer->writeStartElement("single_strands");
-  SB_FOR(ADNPointer<ADNSingleStrand> ss, singleStrandsIndex_) {
-    serializer->writeUnsignedIntElement("ss_idx", nodeIndexer.getIndex(ss()));
-    serializer->writeStringElement("name", ss->GetName());
-    serializer->writeBoolElement("isScaffold", ss->IsScaffold());
-    serializer->writeBoolElement("isCircular", ss->IsCircular());
-    serializer->writeIntElement("num_nts", ss->getNumberOfNucleotides());
+  //serializer->writeStartElement("single_strands");
+  //SB_FOR(ADNPointer<ADNSingleStrand> ss, singleStrandsIndex_) {
+  //  serializer->writeUnsignedIntElement("ss_idx", nodeIndexer.getIndex(ss()));
+  //  serializer->writeStringElement("name", ss->GetName());
+  //  serializer->writeBoolElement("isScaffold", ss->IsScaffold());
+  //  serializer->writeBoolElement("isCircular", ss->IsCircular());
+  //  serializer->writeIntElement("num_nts", ss->getNumberOfNucleotides());
 
-    ADNPointer<ADNNucleotide> nt = ss->GetFivePrime();
+  //  ADNPointer<ADNNucleotide> nt = ss->GetFivePrime();
 
-    serializer->writeStartElement("nucleotides");
-    while (nt != nullptr) {
-      serializer->writeUnsignedIntElement("nt_idx", nodeIndexer.getIndex(nt()));
-      serializer->writeStartElement("nt_data");
+  //  serializer->writeStartElement("nucleotides");
+  //  while (nt != nullptr) {
+  //    serializer->writeUnsignedIntElement("nt_idx", nodeIndexer.getIndex(nt()));
+  //    serializer->writeStartElement("nt_data");
 
-      serializer->writeIntElement("type", nt->GetType());
-      serializer->writeStringElement("pos", ADNAuxiliary::SBPositionToString(nt->GetPosition()));
-      serializer->writeStringElement("bbPos", ADNAuxiliary::SBPositionToString(nt->GetBackbonePosition()));
-      serializer->writeStringElement("scPos", ADNAuxiliary::SBPositionToString(nt->GetSidechainPosition()));
-      serializer->writeStringElement("e1", ADNAuxiliary::UblasVectorToString(nt->GetE1()));
-      serializer->writeStringElement("e2", ADNAuxiliary::UblasVectorToString(nt->GetE2()));
-      serializer->writeStringElement("e3", ADNAuxiliary::UblasVectorToString(nt->GetE3()));
+  //    serializer->writeIntElement("type", nt->GetType());
+  //    serializer->writeStringElement("pos", ADNAuxiliary::SBPositionToString(nt->GetPosition()));
+  //    serializer->writeStringElement("bbPos", ADNAuxiliary::SBPositionToString(nt->GetBackbonePosition()));
+  //    serializer->writeStringElement("scPos", ADNAuxiliary::SBPositionToString(nt->GetSidechainPosition()));
+  //    serializer->writeStringElement("e1", ADNAuxiliary::UblasVectorToString(nt->GetE1()));
+  //    serializer->writeStringElement("e2", ADNAuxiliary::UblasVectorToString(nt->GetE2()));
+  //    serializer->writeStringElement("e3", ADNAuxiliary::UblasVectorToString(nt->GetE3()));
 
-      serializer->writeEndElement();
-      nt = nt->GetNext();
-    }
-    serializer->writeEndElement();
-  }
-  serializer->writeEndElement();
+  //    serializer->writeEndElement();
+  //    nt = nt->GetNext();
+  //  }
+  //  serializer->writeEndElement();
+  //}
+  //serializer->writeEndElement();
 }
 
 void ADNPart::unserialize(SBCSerializer * serializer, const SBNodeIndexer & nodeIndexer, const SBVersionNumber & sdkVersionNumber, const SBVersionNumber & classVersionNumber)
 {
   //SBStructuralModel::unserialize(serializer, nodeIndexer, sdkVersionNumber, classVersionNumber);
 
-  int numSss = serializer->readIntElement();
+  //int numSss = serializer->readIntElement();
 
-  serializer->readStartElement();  // begin single strands
+  //serializer->readStartElement();  // begin single strands
 
-  // build bottom-up
-  for (int i = 0; i < numSss; ++i) {
-    unsigned int id = serializer->readUnsignedIntElement();
-    std::string name = serializer->readStringElement();
-    bool isScaf = serializer->readBoolElement();
-    bool isCirc = serializer->readBoolElement();
-    int numNt = serializer->readIntElement();
+  //// build bottom-up
+  //for (int i = 0; i < numSss; ++i) {
+  //  unsigned int id = serializer->readUnsignedIntElement();
+  //  std::string name = serializer->readStringElement();
+  //  bool isScaf = serializer->readBoolElement();
+  //  bool isCirc = serializer->readBoolElement();
+  //  int numNt = serializer->readIntElement();
 
-    ADNPointer<ADNSingleStrand> ss = new ADNSingleStrand();
-    RegisterSingleStrand(ss);
-    ss->SetName(name);
-    ss->IsScaffold(isScaf);
-    ss->IsCircular(isCirc);
+  //  ADNPointer<ADNSingleStrand> ss = new ADNSingleStrand();
+  //  RegisterSingleStrand(ss);
+  //  ss->SetName(name);
+  //  ss->IsScaffold(isScaf);
+  //  ss->IsCircular(isCirc);
 
-    serializer->readStartElement();  // begin nucleotides
+  //  serializer->readStartElement();  // begin nucleotides
 
-    for (int j = 0; j < numNt; ++j) {
-      unsigned int nt_id = serializer->readUnsignedIntElement();
-      ADNPointer<ADNNucleotide> nt = new ADNNucleotide();
+  //  for (int j = 0; j < numNt; ++j) {
+  //    unsigned int nt_id = serializer->readUnsignedIntElement();
+  //    ADNPointer<ADNNucleotide> nt = new ADNNucleotide();
 
-      serializer->readStartElement();  // nucleotide data
+  //    serializer->readStartElement();  // nucleotide data
 
-      DNABlocks t = DNABlocks(serializer->readIntElement());
-      SBPosition3 pos = ADNAuxiliary::StringToSBPosition(serializer->readStringElement());
-      SBPosition3 bbPos = ADNAuxiliary::StringToSBPosition(serializer->readStringElement());
-      SBPosition3 scPos = ADNAuxiliary::StringToSBPosition(serializer->readStringElement());
-      auto e1 = ADNAuxiliary::StringToUblasVector(serializer->readStringElement());
-      auto e2 = ADNAuxiliary::StringToUblasVector(serializer->readStringElement());
-      auto e3 = ADNAuxiliary::StringToUblasVector(serializer->readStringElement());
+  //    DNABlocks t = DNABlocks(serializer->readIntElement());
+  //    SBPosition3 pos = ADNAuxiliary::StringToSBPosition(serializer->readStringElement());
+  //    SBPosition3 bbPos = ADNAuxiliary::StringToSBPosition(serializer->readStringElement());
+  //    SBPosition3 scPos = ADNAuxiliary::StringToSBPosition(serializer->readStringElement());
+  //    auto e1 = ADNAuxiliary::StringToUblasVector(serializer->readStringElement());
+  //    auto e2 = ADNAuxiliary::StringToUblasVector(serializer->readStringElement());
+  //    auto e3 = ADNAuxiliary::StringToUblasVector(serializer->readStringElement());
 
-      nt->SetType(t);
-      nt->SetPosition(pos);
-      nt->SetBackbonePosition(bbPos);
-      nt->SetSidechainPosition(scPos);
-      nt->SetE1(e1);
-      nt->SetE2(e2);
-      nt->SetE3(e3);
+  //    nt->SetType(t);
+  //    nt->SetPosition(pos);
+  //    nt->SetBackbonePosition(bbPos);
+  //    nt->SetSidechainPosition(scPos);
+  //    nt->SetE1(e1);
+  //    nt->SetE2(e2);
+  //    nt->SetE3(e3);
 
-      RegisterNucleotideThreePrime(ss, nt);
+  //    RegisterNucleotideThreePrime(ss, nt);
 
-      serializer->readEndElement();  // end nucleotide data
-    }
+  //    serializer->readEndElement();  // end nucleotide data
+  //  }
 
-    serializer->readEndElement();  // end nucleotides
-  }
+  //  serializer->readEndElement();  // end nucleotides
+  //}
 
-  serializer->readEndElement();  // end single strands
+  //serializer->readEndElement();  // end single strands
 
-  // build top-down
+  //// build top-down
 
-  loadedViaSAMSON(true);
+  //loadedViaSAMSON(true);
 }
 
 std::string const & ADNPart::GetName() const
