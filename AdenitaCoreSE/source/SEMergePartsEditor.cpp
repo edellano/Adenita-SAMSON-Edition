c991d1d6207ce37cbac1720242925ef496619703
Haichao Miao <miao@cg.tuwien.ac.at>
2019-02-06 10:40:02 +0100

    added now all icons for current buttons and functions

--- AdenitaCoreSE/source/SEMergePartsEditor.cpp
+++ AdenitaCoreSE/source/SEMergePartsEditor.cpp
@@ -51,7 +51,7 @@ QPixmap SEMergePartsEditor::getLogo() const {
 	// SAMSON Element generator pro tip: this icon will be visible in the GUI title bar. 
 	// Modify it to better reflect the purpose of your editor.
 
-	return QPixmap(QString::fromStdString(SB_ELEMENT_PATH + "/Resource/Icons/merge_parts.png"));
+	return QPixmap(QString::fromStdString(SB_ELEMENT_PATH + "/Resource/Icons/merge.png"));
 
 }
 
