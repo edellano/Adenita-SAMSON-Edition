e918f4b9fdac7d604906f12c272473bad1dc9fa5
edellano <elisa.dellano@gmail.com>
2019-05-12 12:38:22 +0200

    Fixed minor bug in calculation of GC content Fixed bug in sequence generator Fixed bug in create dsDNA strands

--- AdenitaCoreSE/source/SEDSDNACreatorEditor.cpp
+++ AdenitaCoreSE/source/SEDSDNACreatorEditor.cpp
@@ -391,11 +391,13 @@ void SEDSDNACreatorEditor::mouseReleaseEvent(QMouseEvent* event) {
     ADNPointer<ADNPart> part = nullptr;
     if (!circular_) part = generateStrand();
     else part = generateCircularStrand();
-    int test3 = part->GetNumberOfSingleStrands();
-    sendPartToAdenita(part);
-    DASCreatorEditors::resetPositions(positions_);
-    display_ = false;
-    tempPart_ == nullptr;
+    if (part != nullptr) {
+      int test3 = part->GetNumberOfSingleStrands();
+      sendPartToAdenita(part);
+      DASCreatorEditors::resetPositions(positions_);
+      display_ = false;
+      tempPart_ == nullptr;
+    }
   }
 }
 
