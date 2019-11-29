68a01b762446aff87df2e9da334624a427ff4b2e
edellano <elisa.dellano@gmail.com>
2019-11-29 12:53:02 +0100

    Fixing bug with icon of connect editor

--- AdenitaCoreSE/source/SEConnectSSDNAEditor.cpp
+++ AdenitaCoreSE/source/SEConnectSSDNAEditor.cpp
@@ -76,7 +76,7 @@ QPixmap SEConnectSSDNAEditor::getLogo() const {
 	// SAMSON Element generator pro tip: this icon will be visible in the GUI title bar. 
 	// Modify it to better reflect the purpose of your editor.
 
-	return QPixmap(QString::fromStdString(SB_ELEMENT_PATH + "/Resource/Icons/connectSS.png"));
+	return QPixmap(QString::fromStdString(SB_ELEMENT_PATH + "/Resource/Icons/connect.png"));
 
 }
 
